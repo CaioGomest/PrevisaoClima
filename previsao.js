85f9620b9f0ef57af441a5c43d9c7289d7d650b0
Caio Gomes <caiogomesteodoro21@gmail.com>
2023-05-31 01:40:50 -0300

    adicionando condiçao de cidade nao encontrada

--- previsao.js
+++ previsao.js
@@ -2,11 +2,17 @@ const key = "fa10062a0828ab9dc0a6cf2b82dd1ddb";
 
 
 function preencherInfos(data, NameCity) {
-
-    document.querySelector('.cidade').innerHTML = "Tempo em " + NameCity;
-    document.querySelector('.clima').innerHTML = Math.floor( data.main.temp ) + "°C";
-    document.querySelector('.tempo').innerHTML = data.weather[0].description;
-}
+    if (data.cod === "404") {
+      document.querySelector('.cidade').innerHTML = "Cidade não encontrada";
+      document.querySelector('.clima').innerHTML = "";
+      document.querySelector('.tempo').innerHTML = "";
+    } else {
+      document.querySelector('.cidade').innerHTML = "Tempo em " + NameCity;
+      document.querySelector('.clima').innerHTML = Math.floor(data.main.temp) + "°C";
+      document.querySelector('.tempo').innerHTML = data.weather[0].description;
+    }
+  }
+  
 async function cityName() {
     var NameCity = document.querySelector(".input-cidade").value;
     await fetchData(NameCity);
